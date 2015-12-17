2202b7c71619a0531c1721853e2b3a7ab4ba23e8
Ilya <ilya.prihlop@gmail.com>
2015-12-17 17:30:48 +0300

    Initial commit
    First commit. Create project.

---
+++ 1c-rac-utilite.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+#source ./config.cfg
+CL_STRING=$(/opt/1C/v8.3/i386/rac cluster list | grep 'cluster  *')
+CLUSTER=${CL_STRING:32}
+DBUSER=                           #PostgreSQL database user
+DBPASS=                           #PostgreSQL database password
+OPTION=1                          #Default option select to resolve error
+#function for question with answer Y/N
+question() {
+  while echo -n "$1 [Y/N] " && read answer || true && ! grep -e '^[YyNn]$' <<< $answer > /dev/null; do echo "Введите N(n) либо Y(y)! "; done
+  return $(tr 'YyNn' '0011' <<< $answer)
+}
+#function to resolve 1c db information
+dbuid() {
+read -p "Введите имя базы с которой желаете работать, или 0(ноль) для отмены: " DBNAME
+DBINFO=$(/opt/1C/v8.3/i386/rac infobase --cluster=$CLUSTER summary list | grep -w -B 1 $DBNAME | grep infobase)
+DBUID=${DBINFO:11}
+}
+echo
+#echo -n "Введите логин пользователя 1С с правами администратора: "
+#read LOGIN
+#echo -n "Введите пароль пользователя 1С: "
+#read -s PASSWD
+clear
+echo
+echo
+echo "Информация о локальном кластере сервера 1С:"
+echo
+/opt/1C/v8.3/i386/rac cluster list
+#echo "$CLUSTER"
+until [ $OPTION -eq 0 ]
+do
+echo
+echo "Выберите для продолжения: "
+echo "1) Вывести список БД"
+echo "2) Добавить БД"
+echo "3) Удалить БД"
+echo "4) Выполнить веб публикацию серверной БД"
+echo "5) Удалить веб публикацию серверной БД"
+echo "0) Выход"
+echo
+read -p "Выбраная опция: " OPTION
+echo
+case "$OPTION" in
+   1) clear
+/opt/1C/v8.3/i386/rac infobase --cluster=$CLUSTER summary list ;;
+   2) clear
+read -p "Введите имя создаваемой базы: " DBNAME
+/opt/1C/v8.3/i386/rac infobase create --cluster=$CLUSTER --name=$DBNAME --create-database --dbms=PostgreSQL --db-server=127.0.0.1 --db-name=$DBNAME --locale=ru --db-user=$DBUSER --db-pwd=$DBPASS --license-distribution=allow > /dev/null
+echo
+echo "БД успешна создана";;
+   3) clear
+dbuid
+while [ -z "$DBUID" ]
+do
+if [ $DBNAME != "0" ]; then
+  echo
+  echo "БД с таким именем не найдена! Повторите ввод."
+  dbuid
+fi
+done
+if [ $DBNAME != "0" ]; then
+question "Уверены что хотите удалить БД с именем $DBNAME ?" && /opt/1C/v8.3/i386/rac infobase drop --cluster=$CLUSTER --infobase=$DBUID --drop-database
+fi
+echo
+echo "БД $DBNAME успешно удалена!";;
+   4) clear
+dbuid
+while [ -z "$DBUID" ]
+do
+if [ $DBNAME != "0" ]; then
+  echo
+  echo "БД с таким именем не найдена! Повторите ввод."
+  dbuid
+fi
+done
+if [ $DBNAME != "0" ]; then
+question "Желаете выполнить веб-публикацию базы с именем $DBNAME ?" && echo "/opt/1C/v8.3/i386/webinst -publish -apache22 -wsdir $DBNAME -dir '/var/www/$DBNAME' -connStr 'Srvr="localhost";Ref="$DBNAME";' -confPath /etc/apache2/apache2.conf" | bash
+sudo service apache2 reload > /dev/null
+fi
+echo;;
+   5) clear
+dbuid
+while [ -z "$DBUID" ]
+do
+if [ $DBNAME != "0" ]; then
+  echo
+  echo "БД с таким именем не найдена! Повторите ввод."
+  dbuid
+fi
+done
+if [ $DBNAME != "0" ]; then
+question "Желаете УДАЛИТЬ веб-публикацию базы с именем $DBNAME ?" && echo "/opt/1C/v8.3/i386/webinst -delete -apache22 -wsdir $DBNAME -dir '/var/www/$DBNAME' -connStr 'Srvr="localhost";Ref="$DBNAME";' -confPath /etc/apache2/apache2.conf" | bash
+sudo service apache2 reload > /dev/null
+fi
+echo;;
+   0) exit ;;
+   *) echo
+echo "Неверно выбрана опция.";;
+
+esac
+done
+exit 0
